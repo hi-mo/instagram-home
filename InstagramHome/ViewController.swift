4cc62e7b66aeddeacd8acebfa13ba921b83a462f
hi-mo <hiroki.morita1020@gmail.com>
2023-01-07 19:26:45 +0900

    Initial Commit

---
+++ InstagramHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramHome
+//
+//  Created by 森田大貴 on 2023/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
